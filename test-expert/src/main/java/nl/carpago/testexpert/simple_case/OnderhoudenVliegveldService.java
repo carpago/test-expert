e6abcba13be141a787638afd2a1ebf6b79ebe9c9
Raymond Loman <raymond@raymondloman.nl>
2012-10-26 22:11:56 +0200

    stuk verder weer. ant build slaagt. unittesten moeter er nog in

--- test-expert/src/main/java/nl/carpago/testexpert/simple_case/OnderhoudenVliegveldService.java
+++ test-expert/src/main/java/nl/carpago/testexpert/simple_case/OnderhoudenVliegveldService.java
@@ -3,12 +3,12 @@ package nl.carpago.testexpert.simple_case;
 import nl.carpago.testexpert.Person;
 import nl.carpago.testexpert.annotation.CreateUnittest;
 
+
 public class OnderhoudenVliegveldService {
 
 	public OnderhoudenVliegveldService() {
 		// TODO Auto-generated constructor stub
 	}
-	
 	@CreateUnittest(in="person", out="anotherPerson")
 	public Person getPerson(Person person) {
 		Person result = new Person("Jane Doe", 45);
