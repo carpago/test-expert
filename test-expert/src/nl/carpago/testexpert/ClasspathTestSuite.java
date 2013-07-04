333e4ecb5766863b77220152c035a7a893e7a73b
Raymond Loman <raymond@raymondloman.nl>
2013-07-04 15:26:49 +0200

    removed

--- test-expert/src/nl/carpago/testexpert/ClasspathTestSuite.java
+++
@@ -1,9 +0,0 @@
-package nl.carpago.testexpert;
-
-import org.junit.extensions.cpsuite.ClasspathSuite;
-import org.junit.runner.RunWith;
-@RunWith(ClasspathSuite.class)
-
-public class ClasspathTestSuite {
-	
-}
